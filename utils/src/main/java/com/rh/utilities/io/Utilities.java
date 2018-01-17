349be801637d32363a033be1127dc83cae345712
robert.hanaway <robert.hanaway@synchronoss.com>
2018-01-17 10:28:43 +0000

    Added unit tests for copy

--- utils/src/main/java/com/rh/utilities/io/Utilities.java
+++ utils/src/main/java/com/rh/utilities/io/Utilities.java
@@ -1,29 +1,38 @@
 package com.rh.utilities.io;
 
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Created by robert.hanaway on 16/01/2018.
  */
 
 public class Utilities {
     static final int EOF = -1;
-    static final int DEFAULT_BUFER_SIZE = 1024 * 32;
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 32;
 
     private long read;
     private long written;
-    private final byte[] buffer;
+
     private final Callback callback;
-
+    private final int bufferSize;
     public interface Callback {
-
+        int getEmitThreshold();
+        void emit(long current, long read);
+        void error(Exception exception);
+        void complete(long totalRead);
     }
 
     public Utilities(final int bufferSize, final Callback callback) {
-        buffer = new byte[bufferSize];
+        this.bufferSize = bufferSize;
         this.callback = callback;
     }
 
     public Utilities(final Callback callback) {
-        this(DEFAULT_BUFER_SIZE, callback);
+        this(DEFAULT_BUFFER_SIZE, callback);
     }
 
     public Utilities() {
@@ -46,4 +55,53 @@ public class Utilities {
         read = 0;
     }
 
+    public static void closeQuitely(final InputStream inputStream) {
+        try {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        } catch (Exception e) {
+            //Suppressed
+        }
+    }
+
+    public static void closeQuitely(final OutputStream outputStream) {
+        try {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        } catch (Exception e) {
+            //Suppressed
+        }
+    }
+
+    public void copy(@NonNull InputStream inputStream, @NonNull OutputStream outputStream) {
+        copy(inputStream, outputStream, callback);
+    }
+
+    public void copy(@NonNull InputStream inputStream, @NonNull OutputStream outputStream, final Callback callback) {
+        int emitThreshold = callback != null ? callback.getEmitThreshold() : 0;
+        byte[] buffer = new byte[bufferSize];
+        long totalRead = 0;
+        long lastEmit = 0;
+        int readFromBuffer;
+        try {
+            while (EOF != (readFromBuffer = inputStream.read(buffer))) {
+                read += readFromBuffer;
+                outputStream.write(buffer, 0, readFromBuffer);
+                written += readFromBuffer;
+                totalRead += readFromBuffer;
+                if ((callback != null) && ((totalRead - lastEmit) >= emitThreshold)) {
+                    callback.emit(totalRead, readFromBuffer);
+                }
+            }
+            if (callback != null) {
+                callback.complete(totalRead);
+            }
+        } catch (Exception e) {
+            if (callback != null) {
+                callback.error(e);
+            }
+        }
+    }
 }
---
+++ utils/src/test/java/com/rh/utilities/io/UtilitiesTest.java
@@ -0,0 +1,138 @@
+package com.rh.utilities.io;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by robert.hanaway on 17/01/2018.
+ */
+
+public class UtilitiesTest {
+    static int BUFFER_SIZE = 123;
+    static int EMIT_THRESHOLD = 121;
+    static int INPUT_STREAM_SIZE = 1024;
+    class MyCallback implements Utilities.Callback {
+        private long current;
+        private long read;
+        private long total;
+        private Exception exception;
+        int emitThreshold;
+        private int emitCount;
+
+        public MyCallback(int emitThreshold) {
+            this.emitThreshold = emitThreshold;
+        }
+
+        @Override
+        public int getEmitThreshold() {
+            return emitThreshold;
+        }
+
+        @Override
+        public void emit(long c, long r) {
+            emitCount++;
+            current = c;
+            read = r;
+        }
+
+        @Override
+        public void error(Exception e) {
+            exception = e;
+        }
+
+        @Override
+        public void complete(long t) {
+            total = t;
+        }
+
+        public long getCurrent() {
+            return current;
+        }
+
+        public long getRead() {
+            return read;
+        }
+
+        public long getTotal() {
+            return total;
+        }
+
+        public int getEmitCount() {
+            return emitCount;
+        }
+
+        public Exception getException() {
+            return exception;
+        }
+    }
+
+    @Test
+    public void testCopyLargeNoCallback() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(new byte[INPUT_STREAM_SIZE]);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Utilities utilities = new Utilities();
+        Assert.assertEquals(0, utilities.getRead());
+        Assert.assertEquals(0, utilities.getWritten());
+
+        utilities.copy(byteArrayInputStream, byteArrayOutputStream);
+
+        Assert.assertEquals(INPUT_STREAM_SIZE, utilities.getRead());
+        Assert.assertEquals(INPUT_STREAM_SIZE, utilities.getWritten());
+
+        utilities.clearWritten();
+        utilities.clearRead();
+
+        Assert.assertEquals(0, utilities.getRead());
+        Assert.assertEquals(0, utilities.getWritten());
+
+
+        Utilities.closeQuitely(byteArrayInputStream);
+        Utilities.closeQuitely(byteArrayOutputStream);
+    }
+
+    @Test
+    public void testCopyLargeCallback() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(new byte[INPUT_STREAM_SIZE]);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        MyCallback myCallback = new MyCallback(EMIT_THRESHOLD);
+        Utilities utilities = new Utilities(BUFFER_SIZE, myCallback);
+        Assert.assertEquals(0, utilities.getRead());
+        Assert.assertEquals(0, utilities.getWritten());
+
+        utilities.copy(byteArrayInputStream, byteArrayOutputStream);
+
+        Assert.assertEquals(INPUT_STREAM_SIZE, utilities.getRead());
+        Assert.assertEquals(INPUT_STREAM_SIZE, utilities.getWritten());
+        Assert.assertNull(myCallback.getException());
+        Assert.assertEquals((INPUT_STREAM_SIZE/BUFFER_SIZE) + 1, myCallback.getEmitCount());
+        utilities.clearWritten();
+        utilities.clearRead();
+
+        Assert.assertEquals(0, utilities.getRead());
+        Assert.assertEquals(0, utilities.getWritten());
+
+
+        Utilities.closeQuitely(byteArrayInputStream);
+        Utilities.closeQuitely(byteArrayOutputStream);
+    }
+
+    @Test
+    public void testCopyLargeException() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(new byte[INPUT_STREAM_SIZE]);
+        ByteArrayOutputStream byteArrayOutputStream = null;
+
+        MyCallback myCallback = new MyCallback(EMIT_THRESHOLD);
+        Utilities utilities = new Utilities(BUFFER_SIZE, myCallback);
+
+
+        utilities.copy(byteArrayInputStream, byteArrayOutputStream);
+        Assert.assertEquals(BUFFER_SIZE, utilities.getRead());
+        Assert.assertEquals(0, utilities.getWritten());
+        Assert.assertNotNull(myCallback.exception);
+    }
+}
